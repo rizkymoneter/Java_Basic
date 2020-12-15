48d61d78d1eeda061a3aeab5ded4d11acc709721
rizkymoneter <rmoneter@gmail.com>
2020-12-15 10:42:43 +0700

    2_Commit

--- src/com/dicoding/javafundamental/arrayduadimensi/arrayduadimensi.java
+++ src/com/dicoding/javafundamental/arrayduadimensi/arrayduadimensi.java
@@ -2,6 +2,33 @@ package com.dicoding.javafundamental.arrayduadimensi;
 
 public class arrayduadimensi {
     public static void main(String[] args) {
-        
+
+        char[][] arrChar = new char[2][];
+
+        arrChar[0] = new char[2];
+        arrChar[1] = new char[3];
+
+        // Dimensi 1 yang indeksnya 0 memiliki panjang elemen 2
+
+        arrChar[0][0] = 'A';
+        arrChar[0][1] = 'B';
+
+        // Dimensi 1 yang indeksnya 1 memiliki panjang elemen 2
+
+        arrChar[1][0] = 'C';
+        arrChar[1][1] = 'D';
+        arrChar[1][2] = 'E';
+
+        // Tampilkan semua data dari dimensi 1 yang memiliki indeks 0
+
+        System.out.println("Dimensi 1 dengan indeks 0 dan panjang elenet 2 : " + arrChar[0][0]);
+        System.out.println("Dimensi 1 dengan indeks 0 dan panjang elenet 2 : " + arrChar[0][1]);
+
+        // Tampilkan semua data dari dimensi 1 yang memiliki indeks 1
+
+        System.out.println("Dimensi 1 dengan indeks 1 dan panjang elenet 2 : " + arrChar[1][0]);
+        System.out.println("Dimensi 1 dengan indeks 1 dan panjang elenet 2 : " + arrChar[1][1]);
+        System.out.println("Dimensi 1 dengan indeks 1 dan panjang elenet 2 : " + arrChar[1][2]);
+
     }
 }
