5aa0e35722390566d27ced8b0dbe55aba9e16124
rizkymoneter <rmoneter@gmail.com>
2021-05-28 23:56:48 +0700

    6_commit

---
+++ src/com/dicoding/javafundamental/accessmodifier/KelasA.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.accessmodifier;
+
+public class KelasA {
+    private int memberA = 5;
+    private int functionA() {
+        return memberA;
+    }
+    int functionB() {
+        // Pemanggilan private member dan private function
+        int hasil = functionA() + memberA;
+        return hasil;
+    }
+}
---
+++ src/com/dicoding/javafundamental/accessmodifier/Main.java
@@ -0,0 +1,8 @@
+package com.dicoding.javafundamental.accessmodifier;
+
+public class Main {
+    public static void main(String[] args) {
+        KelasA kelasA = new KelasA();
+        System.out.println(kelasA.functionB());
+    }
+}
