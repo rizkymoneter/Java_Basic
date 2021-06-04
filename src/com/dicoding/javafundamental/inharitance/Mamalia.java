a46eb9ee8d57f3c6c055f0b1c660639cad30789d
rizkymoneter <rmoneter@gmail.com>
2021-06-04 11:04:55 +0700

    8_commit

---
+++ src/com/dicoding/javafundamental/inharitance/Mamalia.java
@@ -0,0 +1,5 @@
+package com.dicoding.javafundamental.inharitance;
+
+public interface Mamalia {
+    Mamalia mamalia = new Kucing();
+}
---
+++ src/com/dicoding/javafundamental/oop/Hewan.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.oop;
+
+public interface Hewan {
+
+    String RESPIRASI = "oksigen";
+
+    void makan();
+
+
+
+}
